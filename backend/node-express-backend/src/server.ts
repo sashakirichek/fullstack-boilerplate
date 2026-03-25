8c0fb1b4e18545c0684596f913b8da0d2722ed3a
olky <sashakirichek@gmail.com>
2026-03-25 15:47:33 +0200

    artillery script before redis integration. posts mock

--- backend/node-express-backend/src/server.ts
+++ backend/node-express-backend/src/server.ts
@@ -1,6 +1,18 @@
-import express, { Express, Request, Response } from 'express'
+import express, {Express, Request, Response} from 'express'
 
 const app: Express = express()
+
+app.get('/posts', (_req: Request, res: Response) => {
+  res.json({
+    items: [
+      {id: 1, text: 'text1'},
+      {id: 2, text: 'text2'},
+      {id: 3, text: 'text3'},
+      {id: 4, text: 'text4'},
+      {id: 5, text: 'text5'},
+    ],
+  })
+})
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Server is running')
